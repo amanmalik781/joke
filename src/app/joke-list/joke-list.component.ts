1acc060ca2f941f6fba28704c842b863af3e84ae
Amandeep Malik <36561468+amanmalik781@users.noreply.github.com>
2018-06-14 21:48:41 +0530

    property binding and event binding added

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -8,19 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class JokeListComponent implements OnInit {
 
   jokes: Object[];
+  toggle(joke){joke.hide=false;}
 constructor() {
 this.jokes = [
 {
 setup: "What did the cheese say when it looked in the mirror?",
-punchline: "Hello-Me (Halloumi)"
+punchline: "Hello-Me (Halloumi)",
+hide:true
+
 },
 {
 setup: "What kind of cheese do you use to disguise a small horse?",
-punchline: "Mask-a-pony (Mascarpone)"
+punchline: "Mask-a-pony (Mascarpone)",
+hide: true
 },
 {
 setup: "A kid threw a lump of cheddar at me",
-punchline: "I thought ‘That’s not very mature’"
+punchline: "I thought ‘That’s not very mature’",
+hide: true
 },
 ];
 }
