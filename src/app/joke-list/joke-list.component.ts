2183901e9e4fe4faa071fd18ca7aafaf854326e7
Amandeep Malik <36561468+amanmalik781@users.noreply.github.com>
2018-06-23 11:01:24 +0530

    layout

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,34 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+class Joke {
+  public setup: string;
+  public punchline: string;
+  public hide: boolean;
+  constructor(setup: string, punchline: string) {
+  this.setup = setup;
+  this.punchline = punchline;
+  this.hide = true;
+  }
+  toggle() {
+  this.hide = !this.hide;
+  }
+  }
 @Component({
   selector: 'app-joke-list',
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
+
 export class JokeListComponent implements OnInit {
 
-  jokes: Object[];
-  toggle(joke){joke.hide=false;}
+  jokes: Joke[];
+ 
 constructor() {
-this.jokes = [
-{
-setup: "What did the cheese say when it looked in the mirror?",
-punchline: "Hello-Me (Halloumi)",
-hide:true
-
-},
-{
-setup: "What kind of cheese do you use to disguise a small horse?",
-punchline: "Mask-a-pony (Mascarpone)",
-hide: true
-},
-{
-setup: "A kid threw a lump of cheddar at me",
-punchline: "I thought ‘That’s not very mature’",
-hide: true
-},
+this.jokes = [new Joke("What did the cheese say when it looked in the mirror?", "Hello-me(Halloumi)"),
+new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-apony(Mascarpone)"),
+new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not verymature’")
 ];
+
 }
+
 
   ngOnInit() {
   }
